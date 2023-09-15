631474b7f0f99a4f7287490860b5a752904b1380
b00ste <daniel@lukso.io>
2023-09-15 20:50:55 +0300

    chore: add License

--- src/components/License.tsx
+++ src/components/License.tsx
@@ -1,8 +1,38 @@
 export const License = () => {
   return (
-    <div className="bg-white p-4 rounded-lg shadow">
-      <h2 className="text-pastel-blue text-2xl mb-4">License</h2>
-      <p className="text-gray-700">This is sample license text.</p>
+    <div className="bg-pastel-light-pink mt-40 mb-16 p-4 m-8 rounded-lg shadow">
+      <h2 className="text-pastel-blue text-2xl mb-4 font-bold">MIT License</h2>
+      <p className="block m-2 text-gray-700">
+        B00ste ("the Company") hereby grants permission to any person or entity
+        (the "Licensee") obtaining a copy of this software and associated
+        documentation files (the "Software"), to deal in the Software without
+        restriction, including without limitation the rights to use, copy,
+        modify, merge, publish, distribute, sublicense, and/or sell copies of
+        the Software, and to permit persons to whom the Software is furnished to
+        do so, subject to the following conditions:
+      </p>
+      <p className="block m-2 pl-4 text-gray-700">
+        1. The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+      </p>
+      <p className="block m-2 pl-4 text-gray-700">
+        2. The Software is provided "as is," without warranty of any kind,
+        express or implied, including but not limited to the warranties of
+        merchantability, fitness for a particular purpose, and noninfringement.
+        In no event shall the Company or the authors or copyright holders be
+        liable for any claim, damages, or other liability, whether in an action
+        of contract, tort, or otherwise, arising from, out of, or in connection
+        with the Software or the use or other dealings in the Software.
+      </p>
+      <p className="block m-2 pl-4 text-gray-700">
+        3. The Software is open source and freely available to the public. The
+        Licensee is free to use, modify, and distribute the Software without any
+        restrictions.
+      </p>
+      <p className="block m-2 text-gray-700">
+        By using the Software, the Licensee agrees to the terms and conditions
+        of this license.
+      </p>
     </div>
   );
 };
